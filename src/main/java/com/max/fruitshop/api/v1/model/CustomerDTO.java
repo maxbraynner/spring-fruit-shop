f512f147976524678c0889bca5386e9c5a4f6f65
Max Braynner <max.braynner@gmail.com>
2019-01-30 23:50:20 -0300

    Ajuste na serialização de CustomerDTO

--- src/main/java/com/max/fruitshop/api/v1/model/CustomerDTO.java
+++ src/main/java/com/max/fruitshop/api/v1/model/CustomerDTO.java
@@ -1,5 +1,6 @@
 package com.max.fruitshop.api.v1.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,8 @@ public class CustomerDTO {
 
     private String firstName;
     private String lastName;
+
+    @JsonProperty("customer_url")
     private String customerUrl;
 
 }
