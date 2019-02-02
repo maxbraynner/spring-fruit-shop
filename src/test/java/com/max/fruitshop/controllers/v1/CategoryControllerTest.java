9dc35490a77b656f350249ef807666d23794fba1
Max Braynner <max.braynner@gmail.com>
2019-02-01 22:05:26 -0300

    Melhorias nos testes dos endpoints de categorias

--- src/test/java/com/max/fruitshop/controllers/v1/CategoryControllerTest.java
+++ src/test/java/com/max/fruitshop/controllers/v1/CategoryControllerTest.java
@@ -1,20 +1,35 @@
 package com.max.fruitshop.controllers.v1;
 
+import com.max.fruitshop.api.v1.model.CategoryDTO;
 import com.max.fruitshop.services.CategoryService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class CategoryControllerTest {
 
+    private static final String NAME = "Max";
+
     @Mock
     private CategoryService service;
+
+    @InjectMocks
+    private CategoryController controller;
 
     private MockMvc mockMvc;
 
@@ -22,19 +37,38 @@ public class CategoryControllerTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        CategoryController controller = new CategoryController(service);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
     public void getAllCategories() throws Exception {
-        mockMvc.perform(get(CategoryController.BASE_URL))
-                .andExpect(status().isOk());
+        CategoryDTO category1 = new CategoryDTO();
+        category1.setId(1L);
+
+        CategoryDTO category2 = new CategoryDTO();
+        category2.setId(2L);
+
+        List<CategoryDTO> categoryDTOS = Arrays.asList(category1, category2);
+
+        when(service.getAllCategories()).thenReturn(categoryDTOS);
+
+        mockMvc.perform(get(CategoryController.BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.categories", hasSize(2)));
     }
 
     @Test
     public void getByName() throws Exception {
-        mockMvc.perform(get(CategoryController.BASE_URL + "/max"))
-                .andExpect(status().isOk());
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(1L);
+        categoryDTO.setName(NAME);
+
+        when(service.getCategoryByName(NAME)).thenReturn(categoryDTO);
+
+        mockMvc.perform(get(CategoryController.BASE_URL + "/" + NAME)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(NAME)));
     }
 }
